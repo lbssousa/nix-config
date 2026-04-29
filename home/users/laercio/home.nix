ba52883c1bb73337dde34c4f87f735729d333bcf
Laércio de Sousa <laercio@sivali.sousa.nom.br>
2026-04-29 13:12:03 -0300

    home(laercio): define perfil padrao do Konsole

--- home/users/laercio/home.nix
+++ home/users/laercio/home.nix
@@ -19,6 +19,20 @@
     SSH_AUTH_SOCK=$HOME/.var/app/com.bitwarden.desktop/data/.bitwarden-ssh-agent.sock
   '';
 
+  xdg.configFile."konsolerc".text = ''
+    [Desktop Entry]
+    DefaultProfile=default.profile
+  '';
+
+  xdg.dataFile."konsole/default.profile".text = ''
+    [Appearance]
+    Font=JetBrainsMono Nerd Font Mono,14,-1,5,50,0,0,0,0,0
+
+    [General]
+    Name=Default
+    Parent=FALLBACK/
+  '';
+
   dconf.settings = {
     "org/gnome/Ptyxis" = {
       use-system-font = false;
